b3e71ee442d2d2dae9820e394603739e0def1c5c
JaacielBriseo <jaacielbris23@gmail.com>
2022-12-08 23:03:03 -0700

    Tuplas

---
+++ tuplas.py
@@ -0,0 +1,14 @@
+
+tupla = (100, 'hello', ['familia', 'amigos'], {"hello": "hello"})
+print(tupla)
+
+tupla = list(tupla)
+
+tupla.append(200)
+tupla=tuple(tupla)
+print(tupla)
+
+##SETS // No puedes tener datos repetidos
+
+nuestro_set = {21,24,23,14,21,21,29}
+print(nuestro_set)
